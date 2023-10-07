88f2b417872fde991fac4c2127666fb361c346dd
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-10-07 20:39:23 +0530

    Update order.schema.js

--- Express/modules/order/dao/schemas/order.schema.js
+++ Express/modules/order/dao/schemas/order.schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: Number, required: true },
     products: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
         quantity: { type: Number, required: true },
